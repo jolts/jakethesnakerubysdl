f6f3a6ce0cd65bcb3afaec02d246670f3e3d1bfe
Gigamo <gigamo@gmail.com>
2009-02-04 23:19:40 +0100

    cleanup again

--- lib/menu.rb
+++ lib/menu.rb
@@ -10,8 +10,8 @@ module JakeTheSnake
       puts 'in draw()'
       menu_screen = load_image("./img/background_and_wall_menu.bmp")
       apple = load_image("./img/apple_menu.bmp")
-      SDL::Surface.blit(menu_screen, 0,0, 0, 0, surface, 0, 0)
-    #SDL::Surface.blit(apple, 0, 0,0, 0,  surface, 0, 0)
+      SDL::Surface.blit(menu_screen, 0, 0, 0, 0, surface, 0, 0)
+    #SDL::Surface.blit(apple, 0, 0, 0, 0,  surface, 0, 0)
     end
   end
 end
