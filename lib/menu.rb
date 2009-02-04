645a942e01c51e714ddfdca04fc6e99bdc3176f1
Gigamo <gigamo@gmail.com>
2009-02-04 23:18:26 +0100

    fix menu indentation

--- lib/menu.rb
+++ lib/menu.rb
@@ -2,16 +2,16 @@
 require 'lib/game_engine'
 
 module JakeTheSnake
-class Menu < GameEngine
-  def initialize
-  end
+  class Menu < GameEngine
+    def initialize
+    end
 
-  def draw(surface)
-    puts 'in draw()'
-    menu_screen = load_image("./img/background_and_wall_menu.bmp")
-    apple = load_image("./img/apple_menu.bmp")
-    SDL::Surface.blit(menu_screen, 0,0, 0, 0, surface, 0, 0)
+    def draw(surface)
+      puts 'in draw()'
+      menu_screen = load_image("./img/background_and_wall_menu.bmp")
+      apple = load_image("./img/apple_menu.bmp")
+      SDL::Surface.blit(menu_screen, 0,0, 0, 0, surface, 0, 0)
     #SDL::Surface.blit(apple, 0, 0,0, 0,  surface, 0, 0)
+    end
   end
-end
 end
