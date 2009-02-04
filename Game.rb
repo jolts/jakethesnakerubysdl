b06261612dbe74c4a6294b170c6083283f3991d6
jolts <johan@sharpcode.se>
2009-02-04 22:19:54 +0100

    3rd commit

--- Game.rb
+++ Game.rb
@@ -1,21 +1,7 @@
 #!/usr/bin/env ruby
 require 'sdl'
-require 'gameengine'
-
-class Menu
-  def initialize
-    @@choice = 1
-    @@choice_y = 176
-  end
-
-  def draw(surface)
-    puts 'in draw()'
-    menu_screen = load_image("background_and_wall_menu.bmp")
-    apple = load_image("apple_menu.bmp")
-    SDL::Surface.blit($screen, 0, 0, 0, 0, menu_screen, 0, 0)
-    SDL::Surface.blit($screen, 0, 0,0,0, apple, 168, 176)
-  end
-end
+require 'lib/game_engine'
+require 'lib/menu'
 
 class Game
   attr_accessor :state
