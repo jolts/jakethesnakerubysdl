0e094347f1b04e3b94de38211dbfd006bc3f5665
jolts <johan@sharpcode.se>
2009-02-05 02:07:35 +0100

    added updated files

--- lib/new_game.rb
+++ lib/new_game.rb
@@ -6,6 +6,49 @@ module JakeTheSnake
   class NewGame < Sprite
     def initialize
       @snake = Snake.new
+      @finished = false
     end
+
+    def key_pressed(key)
+      puts 'in NewGame.key_pressed()'
+      if key == SDL::Key::UP
+        if @snake.direction != 3
+          @snake.direction = 1
+        end
+      elsif key == SDL::Key::RIGHT
+        if @snake.direction != 4
+          @snake.direction = 2
+        end
+      elsif key == SDL::Key::DOWN
+        if @snake.direction != 1
+          @snake.direction = 3
+        end
+      elsif key == SDL::Key::LEFT
+        if @snake.direction != 2
+          @snake.direction = 4
+        end
+      end
+    end
+
+    def clock_tick
+      @snake.move(@snake.direction)
+      check_collisions
+
+      if @snake.parts_to_add
+        @snake.add_parts(1)
+        @snake.parts_toadd -= 1
+      end
+    end
+
+    def check_collisions
+      puts "in NewGame.check_collisions()"
+    end
+    
+    def draw(surface)
+      green_background = SDL::Surface.new
+      green_background.PixelFormat
+      green_background.mapRGB(0x61,0xAD,0x2F)
+      green_background.fillRect(0, 0, 608, 448)
+
+
   end
-end
