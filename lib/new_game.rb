0784b3a398e3263e202f23efc3fb46d5c469b7bc
jolts <johan@sharpcode.se>
2009-02-05 22:17:52 +0100

    fixed reso

--- lib/new_game.rb
+++ lib/new_game.rb
@@ -36,7 +36,6 @@ module JakeTheSnake
       elsif key == SDL::Key::Q
         $game.state = Menu.new
         $stderr.puts Util.new("yellow").colorize("Q_Q")+" pressed."
-        $stderr.puts Util.new("cyan").colorize("Going back to menu")
       end
     end
 
