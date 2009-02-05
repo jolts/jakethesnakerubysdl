2124ce073615075026604c2c535cf938afd23034
Gigamo <gigamo@gmail.com>
2009-02-05 13:58:31 +0100

    cleaner this way

--- lib/snake.rb
+++ lib/snake.rb
@@ -16,7 +16,7 @@ module JakeTheSnake
       @parts_to_add = 3
       @snake_body = Array.new
 
-      @length.times {
+      @length.times do
         snake_part = Struct.new
         snake_part.head = @head
         snake_part.x = @x
@@ -24,11 +24,11 @@ module JakeTheSnake
         @snake_body << snake_part
         @x -= 16
         @head = false
-      }
+      end
     end
 
     def draw(surface)
-      @snake_body.each { |body_part|
+      @snake_body.each do |body_part|
         if body_part.head
           snake_surface = load_image("./img/player1_head.bmp")
         elsif @rainbow
@@ -37,7 +37,7 @@ module JakeTheSnake
           snake_surface = load_image("./img/player_body.bmp")
         end
         SDL::Surface.blit(snake_surface, 0, 0, 0, 0, surface, body_part.x, body_part.y)
-      }
+      end
     end
   end
 
@@ -47,50 +47,49 @@ module JakeTheSnake
 
     when 1
       i = 1
-      @snake_body.each { |body_part|
+      @snake_body.each do |body_part|
         body_part.x = @snake_body[i-1].x
         body_part.y = @snake_body[i-1].y
         i += 1
-      }
+      end
       @snake_body[0].y -= 16
     when 2 
-      @snake_body.each { |body_part|
+      @snake_body.each do |body_part|
         body_part.x = @snake_body[i-1].x
         body_part.y = @snake_body[i-1].y
         i += 1
-      }
+      end
       @snake_body[0].x += 16
     when 3 
-      @snake_body.each { |body_part|
+      @snake_body.each do |body_part|
         body_part.x = @snake_body[i-1].x
         body_part.y = @snake_body[i-1].y
         i += 1
-      }
+      end
       snake_body[0].y += 16
     when 4
-      @snake_body.each { |body_part|
+      @snake_body.each do |body_part|
         body_part.x = @snake_body[i-1].x
         body_part.y = @snake_body[i-1].y
         i += 1
-      }
+      end
       snake_body[0].x -= 16
     end
   end
 
   def add_parts(parts)
-    parts.times {
+    parts.times do
       snake_part = Struct.new
       snake_part.head = false
       snake_part.x = @snake_body[-1].x
       snake_part.y = @snake_body[-1].y
       @snake_body << snake_part
-    }
+    end
   end
 
   def remove_parts(parts)
-    parts.times {
+    parts.times do
       @snake_body.pop
-    }
+    end
   end
 end
-
