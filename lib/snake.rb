35ed4937844f3015ff14c6e4e7ecbf69e4049c37
jolts <johan@sharpcode.se>
2009-02-05 22:36:06 +0100

    fixed some movement

--- lib/snake.rb
+++ lib/snake.rb
@@ -47,34 +47,34 @@ module JakeTheSnake
 
       case direction
       when 1
-        i = 1
+        i = 0
         @snake_body.each do |body_part|
-          body_part[:x] = @snake_body[i-1][:x]
-          body_part[:y] = @snake_body[i-1][:y]
+          body_part[:x] = @snake_body[i][:x]
+          body_part[:y] = @snake_body[i][:y]
           i += 1
         end
         @snake_body[0][:y] -= 16
       when 2 
-        i = 1
+        i = 0
         @snake_body.each do |body_part|
-          body_part[:x] = @snake_body[i-1][:x]
-          body_part[:y] = @snake_body[i-1][:y]
+          body_part[:x] = @snake_body[i][:x]
+          body_part[:y] = @snake_body[i][:y]
           i += 1
         end
         @snake_body[0][:x] += 16
       when 3 
-        i = 1
+        i = 0
         @snake_body.each do |body_part|
-          body_part[:x] = @snake_body[i-1][:x]
-          body_part[:y] = @snake_body[i-1][:y]
+          body_part[:x] = @snake_body[i][:x]
+          body_part[:y] = @snake_body[i][:y]
           i += 1
         end
         @snake_body[0][:y] += 16
       when 4
-        i = 1
+        i = 0
         @snake_body.each do |body_part|
-          body_part[:x] = @snake_body[i-1][:x]
-          body_part[:y] = @snake_body[i-1][:y]
+          body_part[:x] = @snake_body[i][:x]
+          body_part[:y] = @snake_body[i][:y]
           i += 1
         end
         @snake_body[0][:x] -= 16
