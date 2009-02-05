076ba4c770fc3bf4883ee3b349c73064f52828df
Gigamo <gigamo@gmail.com>
2009-02-05 18:28:49 +0100

    fix methods to class

--- lib/snake.rb
+++ lib/snake.rb
@@ -39,57 +39,57 @@ module JakeTheSnake
         SDL::Surface.blit(snake_surface, 0, 0, 0, 0, surface, body_part.x, body_part.y)
       end
     end
-  end
 
-  def move(direction) 
-    self.direction = direction
-    
-    case direction
-    when 1
-      i = 1
-      @snake_body.each do |body_part|
-        body_part.x = @snake_body[i-1].x
-        body_part.y = @snake_body[i-1].y
-        i += 1
+    def move(direction) 
+      self.direction = direction
+
+      case direction
+      when 1
+        i = 1
+        @snake_body.each do |body_part|
+          body_part.x = @snake_body[i-1].x
+          body_part.y = @snake_body[i-1].y
+          i += 1
+        end
+        @snake_body[0].y -= 16
+      when 2 
+        @snake_body.each do |body_part|
+          body_part.x = @snake_body[i-1].x
+          body_part.y = @snake_body[i-1].y
+          i += 1
+        end
+        @snake_body[0].x += 16
+      when 3 
+        @snake_body.each do |body_part|
+          body_part.x = @snake_body[i-1].x
+          body_part.y = @snake_body[i-1].y
+          i += 1
+        end
+        snake_body[0].y += 16
+      when 4
+        @snake_body.each do |body_part|
+          body_part.x = @snake_body[i-1].x
+          body_part.y = @snake_body[i-1].y
+          i += 1
+        end
+        snake_body[0].x -= 16
       end
-      @snake_body[0].y -= 16
-    when 2 
-      @snake_body.each do |body_part|
-        body_part.x = @snake_body[i-1].x
-        body_part.y = @snake_body[i-1].y
-        i += 1
-      end
-      @snake_body[0].x += 16
-    when 3 
-      @snake_body.each do |body_part|
-        body_part.x = @snake_body[i-1].x
-        body_part.y = @snake_body[i-1].y
-        i += 1
-      end
-      snake_body[0].y += 16
-    when 4
-      @snake_body.each do |body_part|
-        body_part.x = @snake_body[i-1].x
-        body_part.y = @snake_body[i-1].y
-        i += 1
-      end
-      snake_body[0].x -= 16
     end
-  end
 
-  def add_parts(parts)
-    parts.times do
-      snake_part = Struct.new
-      snake_part.head = false
-      snake_part.x = @snake_body[-1].x
-      snake_part.y = @snake_body[-1].y
-      @snake_body << snake_part
+    def add_parts(parts)
+      parts.times do
+        snake_part = Struct.new
+        snake_part.head = false
+        snake_part.x = @snake_body[-1].x
+        snake_part.y = @snake_body[-1].y
+        @snake_body << snake_part
+      end
     end
-  end
 
-  def remove_parts(parts)
-    parts.times do
-      @snake_body.pop
+    def remove_parts(parts)
+      parts.times do
+        @snake_body.pop
+      end
     end
   end
 end
