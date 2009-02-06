02c1825c2b27554509cce2a3e70c18b9b8e8ba41
jolts <johan@sharpcode.se>
2009-02-06 20:49:13 +0100

    fixed so loop updates every tick

--- Game.rb
+++ Game.rb
@@ -8,7 +8,7 @@ require 'lib/high_score'
 module JakeTheSnake
   class Game
     attr_accessor :state
-    attr_reader :screen, :random_height, :random_width 
+    attr_reader :screen, :random_height, :random_width, :running
 
     def initialize(height=640, width=480, full_screen=false)
       @height = height
@@ -16,6 +16,7 @@ module JakeTheSnake
       @full_screen = full_screen
       @random_height = rand(608)
       @random_width = rand(448)
+      @running = true
       @state = Menu.new
       @tick_interval = SDL.delay(50)
       @screen = SDL::Screen.open(@height,@width , 32, SDL::SWSURFACE)
@@ -24,22 +25,24 @@ module JakeTheSnake
     def start_game
       SDL.init(SDL::INIT_VIDEO)
 
-      while true
+      while @running
         next_tick = SDL.get_ticks.to_i + @tick_interval.to_i
-
+        
         while event = SDL::Event.poll
           case event
           when SDL::Event::KeyDown
             @state.key_pressed(event.sym)
+          when SDL::Event::Quit
+            self.running = false
           end
-          @state.clock_tick
-          @state.draw(@screen)
-          @screen.flip          
           
           if SDL.get_ticks.to_i < next_tick
             SDL.delay(next_tick - SDL.get_ticks.to_i)
           end
         end
+        @state.clock_tick
+        @state.draw(@screen)
+        @screen.flip          
       end
     end
   end
