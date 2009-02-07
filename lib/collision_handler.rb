cbe425d30aa35f503cdaf5efdfdc18ba41c96598
Gigamo <gigamo@gmail.com>
2009-02-07 21:35:40 +0100

    add barebone collisionhandler

--- lib/collision_handler.rb
+++ lib/collision_handler.rb
@@ -5,14 +5,25 @@ module JakeTheSnake
     def initialize
     end
 
-    def is_collision(snake_body, sprite)
+    def is_wall_collision(snake_head, wall)
+      if snake_head.x < wall.x or snake_head.y < wall.y or snake_head.x + 16 > wall.x + wall.w or snake_head.y + 16 > wall.y + wall.y
+        true
+      else
+        false
+      end
+    end
+
+    def is_obstacles_collision(snake_body, sprite)
       snake_body.each do |body_part|
         if body_part[:x] == sprite.x && body_part[:y] == sprite.y
           true
         else
-         false
+          false
         end
       end
+    end
+
+    def is_self_snake_collision(snake)
     end
   end
 end
