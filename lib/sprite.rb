a117b939ace112e0882c2a4ca8085a9b652c3a9a
jolts <johan@sharpcode.se>
2009-02-07 12:00:21 +0100

    fixed memory leak in load_image(), due to same image being loaded several times, leading to a infinite loop of mallocs

--- lib/sprite.rb
+++ lib/sprite.rb
@@ -4,12 +4,20 @@ require 'sdl'
 module JakeTheSnake
   class Sprite
     attr_accessor :apple_x, :apple_y, :choice, :direction 
-
+    
+    # Keeps track of already loaded images
+    $image_buffer = Hash.new
+    
     def initialize
     end
 
     def load_image(filename)
-      SDL::Surface.load_bmp(filename).display_format
+      if $image_buffer[filename]
+        # If file has already been loaded, return
+        return $image_buffer[filename]
+      end
+      loaded_image = SDL::Surface.load_bmp(filename).display_format
+      $image_buffer[filename] = loaded_image
     end
 
     def draw(surface)
