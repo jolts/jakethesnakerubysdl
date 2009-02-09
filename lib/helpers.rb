6f4048b9448eb9a647a867f742b1b86c961ba1ee
Gigamo <gigamo@gmail.com>
2009-02-09 12:29:59 +0100

    fix bug! ! !

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -7,11 +7,11 @@ module JakeTheSnake
       time = Time.now.strftime("%T")
 
       log = File.new($log_file, "a")
-      log.write("[#{$time}] #{text}\r\n")
+      log.write("[#{time}] #{text}\r\n")
       log.close
 
       if JakeTheSnake::Verbose
-        $stderr.puts "[#{$time}] #{text}"
+        $stderr.puts "[#{time}] #{text}"
       end
     end
 
@@ -22,7 +22,7 @@ module JakeTheSnake
 
       score_time = Time.now.strftime("%d/%m/%Y, %T")
       score = File.new($score_file, "a")
-      score.write("" << "You scored #{points} points at #{$score_time}\r\n")
+      score.write("" << "You scored #{points} points at #{score_time}\r\n")
       score.close
     end
   end
