f0870b9152970211cc9ce7f6aa2a43dfe087bb3d
Gigamo <gigamo@gmail.com>
2009-03-02 12:58:55 +0100

    Catch :int

--- bin/jake_the_snake.rb
+++ bin/jake_the_snake.rb
@@ -4,9 +4,7 @@ require File.dirname(__FILE__) + '/../lib/game_engine'
 
 begin
   $logger.info('Started Jake')
+  trap(:INT) { $logger.fatal("Caught #{:INT}, exiting") }
   $game = JakeTheSnake::GameEngine.new
   $game.init if $0 == __FILE__
-rescue => err
-  $logger.fatal('Caught Exception, exiting')
-  $logger.fatal(err)
 end
